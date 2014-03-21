cdb9b12570e381713cacd5171498d781adb37fd6
Nathan Trapuzzano <nbtrap@nbtrap.com>
2014-03-21 15:04:46 -0400

    Define two commands, one for expanding as usual, and one for expanding inline.
    Both commands are just wrappers around the core function that does the
    expansion.

--- macroutil.el
+++ macroutil.el
@@ -1,41 +1,67 @@
-(defun macroutil-macroexpand-sexp-at-point (&optional all)
-  "Show the macro expansion of the form after point in another \
-window.
-With non-nil prefix argument ALL, expand subforms as well."
-  (interactive "P")
+;;;; -*- lexical-binding: t -*-
+(defun macroutil--macroexpand-sexp-at-point (all inline)
   (condition-case err
-      (save-excursion
-        (let* ((obj (read (current-buffer)))
-               (buf (get-buffer-create "*Macro expansion*"))
-               (already-there (eq (current-buffer) buf)))
-          ;; When we're already in the macro expansion buffer, we expand the
-          ;; macro in place.
-          (when (not already-there)
-            (pop-to-buffer buf))
-          (view-mode 0)
-          (widen)
-          (if (not already-there)
-              (erase-buffer)
-            (delete-region (point) (progn (backward-sexp) (point))))
-          (pp (if all (macroexpand-all obj) (macroexpand obj))
-              (current-buffer))
-          (when already-there
-            (backward-char)
-            (when (looking-at-p "$")
-              (delete-char 1))
-            (backward-sexp)
-            (indent-region (point) (progn (forward-sexp) (point))))
-          (emacs-lisp-mode)
-          (view-mode)
-          (when (not already-there)
-            (goto-char (point-min)))))
+      (let* ((obj (save-excursion
+                    (read (current-buffer))))
+             (macroexpanded-obj
+              (funcall (if all 'macroexpand-all 'macroexpand) obj))
+             (beg (save-excursion
+                    (forward-sexp)
+                    (forward-sexp -1)
+                    (point)))
+             (end (save-excursion
+                    (forward-sexp)
+                    (point))))
+        (when (> (point) beg)
+          ;; Point is not at a sexp boundary.  Make sure we only treat what
+          ;; follows point as the sexp to be expanded.
+          (setq beg (point)))
+        (cond
+         (inline
+           (save-excursion
+             (let ((inhibit-read-only
+                    (if (string= (buffer-name (current-buffer))
+                                 "*Macro expansion*")
+                        t
+                      inhibit-read-only)))
+               (delete-region beg end)
+              (goto-char beg)
+              (pp macroexpanded-obj (current-buffer))
+              (when (bolp)
+                (delete-char -1)))))
+         (t
+          (let* ((buf (get-buffer-create "*Macro expansion*"))
+                 (already-there (eq buf (current-buffer))))
+            (when (not already-there)
+              (pop-to-buffer buf))
+            (use-local-map (copy-keymap (or (current-local-map)
+                                            (make-sparse-keymap))))
+            (view-mode 0)
+            (widen)
+            (erase-buffer)
+            (pp macroexpanded-obj (current-buffer))
+            (goto-char (point-min))
+            (emacs-lisp-mode)
+            (view-mode)
+            (local-set-key [remap undo]
+             (lambda ()
+               (interactive)
+               (let ((inhibit-read-only t))
+                 (call-interactively 'undo))))))))
     (error
-     (message "Can't do macro expansion: %S" err))))
+     (message "Can't do macro expansion: %s (%s)"
+              (error-message-string err) (car err)))))
+
+(defun macroutil-macroexpand-sexp-at-point (&optional all)
+  (interactive "P")
+  (macroutil--macroexpand-sexp-at-point all nil))
+
+(defun macroutil-macroexpand-sexp-at-point-inline (&optional all)
+  (interactive "P")
+  (macroutil--macroexpand-sexp-at-point all t))
 
 (define-minor-mode macroutil-minor-mode "" nil nil
   (list (cons (kbd "C-c m") 'macroutil-macroexpand-sexp-at-point)
-        (cons (kbd "C-c M-m") (lambda ()
-                                (interactive)
-                                (macroutil-macroexpand-sexp-at-point t)))))
+        (cons (kbd "C-c M-m") 'macroutil-macroexpand-sext-at-point-inline)))
 
 (provide 'macroutil)
